386593747b692c4584adeb38ae6dd3329fce34ef
Pietu R <pietu@smartly.io>
2021-12-18 14:56:38 +0200

    main setup

---
+++ src/level.rs
@@ -0,0 +1,5 @@
+use macroquad::prelude::*;
+
+pub fn draw_level() {
+    draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+mod config;
+mod level;
+use crate::config as conf;
+use crate::{config::window_conf, level::draw_level};
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let camera = conf::camera_conf();
+    set_camera(&camera);
+    loop {
+        draw_level();
+        next_frame().await
+    }
 }
